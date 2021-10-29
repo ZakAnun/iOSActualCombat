41ea4319ee5671ad084eebb7255ede4d81723e1a
liminglin <liminglin@youzan.com>
2021-10-29 17:16:57 +0800

    初次提交

---
+++ iOSActualCombat/iOSActualCombatApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSActualCombatApp.swift
+//  iOSActualCombat
+//
+//  Created by zak on 2021/10/29.
+//
+
+import SwiftUI
+
+@main
+struct iOSActualCombatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
